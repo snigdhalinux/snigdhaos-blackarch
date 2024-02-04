6cc33344a8c6788d84e07e14baedfc37fcb9b375
Eshan Roy (Eshanized) <148610067+eshanized@users.noreply.github.com>
2024-02-05 03:34:33 +0530

    updating profile ✅

--- archiso-blackarch/profiledef.sh
+++ archiso-blackarch/profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
+iso_name="snigdhaos-blackarch"
 iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue DVD"
+iso_publisher="Snigdha OS <https://archlinux.org>"
+iso_application="Snigdha OS Live/Rescue DVD"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
